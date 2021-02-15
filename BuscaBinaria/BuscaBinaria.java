c675250d51dbf1175e76671b53f648739181a9d6
niellytonmarques <niellytonmarques20152015@gmail.com>
2021-02-15 12:03:34 -0300

    classe de testes para resposta da atividade

--- BuscaBinaria/BuscaBinaria.java
+++ BuscaBinaria/BuscaBinaria.java
@@ -1,21 +1,26 @@
+import javax.print.attribute.standard.MediaSize.NA;
+
 public class BuscaBinaria {
 
   public int busca(int[] array, int chave) {
     int i = -1;
     int a = 0;
     int b = array.length;
-
-    while(a <= b) {
-      int meio = (a +b) / 2;
-      if(array[meio] < chave) {
-        a = meio + 1;
-      } else if(array[meio] > chave) {
-        b = meio - 1;
-      } else if(array[meio] == chave) {
-        i = meio;
-        break;
+    if (array.length == 0) {
+      return i;
+    } else {
+      while (a <= b) {
+        int meio = (a + b) / 2;
+        if (array[meio] < chave) {
+          a = meio + 1;
+        } else if (array[meio] > chave) {
+          b = meio - 1;
+        } else if (array[meio] == chave) {
+          i = meio;
+          break;
+        }
       }
+      return i;
     }
-    return i;  
   }
 }
---
+++ BuscaBinaria/BuscaBinariaTeste.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+// Niellyton  Marques
+
+public class BuscaBinariaTeste {
+
+    @Test
+    public void inicioDoArray(){
+        int[] inicio = {0, 1, 3, 4, 10, 20, 44};
+        int chave = 0;
+        BuscaBinaria buscar = new BuscaBinaria();
+        assertEquals(0, buscar.busca(inicio, chave));
+    }
+
+    @Test
+    public void meioArray(){
+        int[] meio = {0, 25, 27, 33, 40};
+        int chave = 27;
+        BuscaBinaria buscar = new BuscaBinaria();
+        assertEquals(2, buscar.busca(meio, chave));
+    }
+
+    @Test
+    public void fimArray(){
+        int[] fim = {200, 1500, 2000, 2021};
+        int chave = 2021;
+        BuscaBinaria buscar = new BuscaBinaria();
+        assertEquals(3, buscar.busca(fim, chave));
+    }
+
+    @Test
+    public void arrayVazio(){
+        int[] teste = {};
+        int chave = 33333333;
+        BuscaBinaria buscar = new BuscaBinaria();
+        assertEquals(-1, buscar.busca(teste, chave));
+    }
+
+    @Test
+    public void chaveForaDoArray(){
+        int[] array = {2, 3, 33, 555, 3033};
+        int chave = 0;
+        BuscaBinaria buscar = new BuscaBinaria();
+        assertEquals(-1, buscar.busca(array, chave));
+    }
+
+}
